240c3eff9e50bc92ee78cb113264a7238ede8588
Charles2205 <cewudzie0@gmail.com>
2023-02-08 12:35:22 +0000

    Halfway done!!✨

--- program.py
+++ program.py
@@ -1,4 +1,5 @@
 from colors import GREEN,RED,WHITE,YELLOW
+from random import randint
 def welcome_screen():
     print(YELLOW,'*** WELCOME TO GAME GUESSER ***' )
     user_input = int(input(WHITE+'1. Start Game \n2. About Game Guesser \n3. Exit Application \n\nChoose any valid option: ' ))
@@ -29,20 +30,38 @@ def invalid_option():
 
 def difficulty():
     print(YELLOW,'*** Choose Level ***')
-    user_response=int(input(WHITE+' 1.Beginner (1-10)\n 2.Intermediate (1-100)\n 3.Hard (1-1500)\n\n 4.Back to menu\n'))
+    user_response=int(input(WHITE+' 1.Beginner (1-10)\n 2.Intermediate (1-100)\n 3.Hard (1-1500)\n\n 4.Back to menu\n Enter a valid number: '))
     validate_user_response(user_response)
 
 def validate_user_response(user_response):
     if user_response ==1:
-        pass
+        start_game(10,2)
     elif user_response ==2:
-        pass
+        start_game(100,5)
     elif user_response ==3:
-        pass
+        start_game(500,6)
     elif user_response ==4:
-        pass
+        welcome_screen()
     else:
          invalid_option()
+
+def generate_random_number(endpoint):
+    return randint(1,endpoint)
+
+def start_game(user_level,user_attempt):
+    generated_number = generate_random_number(user_level)
+
+    for trial in range(user_attempt):
+        user_guess=int(input('\nYou have 2 guess in this difficulty...\nGuess a number: '))
+        if user_guess == generated_number:
+            print(GREEN ,'Hurray!! You guess right.👏🎊')
+            break
+        elif user_guess > generated_number:
+            print(RED,'You guessed Too high.Try again😒')
+        elif user_guess < generated_number:        
+            print(RED,'Sorry guess too low.Try again.😒')
+
+
 
 
 
